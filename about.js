46338590ecc61d2e260d28c79d19072bfb4df423
farelTI <123742844+farelTI@users.noreply.github.com>
2024-07-01 18:24:27 +0700

    about.js

---
+++ about.js
@@ -0,0 +1,38 @@
+// components/about.js
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const About = ({ route }) => {
+  const { gambar, judul, telpon } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Image source={gambar} style={styles.image} />
+      <Text style={styles.title}>{judul}</Text>
+      <Text style={styles.phone}>{telpon}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  phone: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+});
+
+export default About;
